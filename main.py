40876d526ec8c30b5c622acb71fc79cfa47a81f1
Cameron Colleran <cameroncolleran3@berkeley.edu>
2022-06-26 22:07:09 -0700

    add replying to mentions with weather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from time import sleep
 from utils import *
 def main():
     for mention in get_mentions():
-        respond_to_mention(mention)
-main()
+        respond_to_mention(mention)       
+while True:
+    main()
+    sleep(15) # make replies every 15 seconds
--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
+from dataclasses import dataclass
 import re
+from readline import get_endidx
+from webbrowser import get
 import tweepy
 import config
+import requests
+
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+@dataclass
+class Weather:
+    temp: str
+    description: str
+    humidity: str
 
 def get_last_tweet_id(file):
     with open(file, 'r') as openfile:
@@ -15,13 +28,7 @@ def set_last_tweet_id(id, file):
     f_write.write(str(id))
     f_write.close()
 
-
 def get_mentions():
-    auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
-    auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-    
-    # DEV_NOTE: first tweet id is 1540852511149461505 for testing purposes  
     most_recent_id = get_last_tweet_id(config.PREV_TWEET_ID_FILENAME)
     if most_recent_id != '': # no tweets yet
         mentions = api.mentions_timeline(since_id = get_last_tweet_id(config.PREV_TWEET_ID_FILENAME))
@@ -30,8 +37,27 @@ def get_mentions():
     return reversed(mentions)
 
 def respond_to_mention(mention):
+    # TODO: add more robust string parsing (i.e. get the city from a 'City, State' format)
     location = (re.sub('@[\w]+', '', mention.text)).strip() 
     print(location)
     id = mention.id
     set_last_tweet_id(id, config.PREV_TWEET_ID_FILENAME)
-    # TODO: add response
+    weather = get_weather(location)
+    if weather == None:
+        weather_message = 'Sorry, I\'m not familiar with that city! Make sure your tweet is formatted with just the city name after your mention (i.e. New York)'
+    else:
+        # TODO: add more information to the message
+        weather_message = f'Currently {str(weather.temp)}°F in {location} with a humidity of {weather.humidity}% and {weather.description}'
+    api.update_status(status = weather_message, in_reply_to_status_id = id, auto_populate_reply_metadata = True)
+
+def get_weather(city_name):
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={config.OPEN_WEATHER_API_KEY}'
+    response = requests.get(request_url).json()
+    try:
+        temp = response['main']['temp']
+        desc = response['weather'][0]['description']
+        humiditiy = response['main']['humidity']
+        output = Weather(temp=temp, description=desc, humidity=humiditiy)
+        return output
+    except:
+        return None
